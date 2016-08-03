5fb04709f8edb89d3b6143c5251839e594161e4f
XHuan <xuanhuanmu95@gmail.com>
2016-08-03 22:38:49 +0700

    base project

---
+++ TourOnline/TourOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TourOnline.MvcApplication" Language="C#" %>
---
+++ TourOnline/TourOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TourOnline.Startup))]
+namespace TourOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
